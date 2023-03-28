226d49a6e601ac1b612909b59159551d5f47b508
Kim-SaeByeol <toquf345@gmail.com>
2023-03-28 18:37:18 +0900

    공부용

--- src/main/java/chap_03/Pg_123.java
+++ src/main/java/chap_03/Pg_123.java
@@ -4,10 +4,10 @@ public class Pg_123 {
     public static void main(String[] args) {
 
 
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                System.out.print(i + "*" + j + "=" + i * j);
-                System.out.print('\t');
+        for (int i = 1; i < 10; i++) {      // 9번 반복
+            for (int j = 1; j < 10; j++) {  //9번 반복
+                System.out.print(i + "*" + j + "=" + i * j);    // 1의 값이 변할때 마다 j 가 1~9 까지 출력.
+                System.out.print('\t');     //tab
             }
             System.out.println();
         }
