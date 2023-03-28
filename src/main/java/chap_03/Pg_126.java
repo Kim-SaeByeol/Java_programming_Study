0d9f9486ddccf11227df1f0bec5c0b5acbe25d5e
Kim-SaeByeol <toquf345@gmail.com>
2023-03-28 18:46:47 +0900

    공부용

--- src/main/java/chap_03/Pg_126.java
+++ src/main/java/chap_03/Pg_126.java
@@ -7,11 +7,11 @@ public class Pg_126 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("exit를 입력하면 종료합니다.");
-        while (true) {
+        while (true) {          //무한 반복
             System.out.print(">>");
             String text = sc.nextLine();
-            if(text.equals("exit"))
-                break;
+            if(text.equals("exit"))     //text.equals()는 () 안에 문자열이 입력되면 true 값을 리턴. 즉슨, exit 를 입력시 if 가 실행됨.
+                break;      //반복문을 벗어남.
         }
         System.out.println("종료합니다...");
         sc.close();
