4b16b951b9241fad1ea2b6a80570a89699a8da2f
Kim-SaeByeol <toquf345@gmail.com>
2023-03-28 18:34:53 +0900

    공부용

--- src/main/java/chap_03/Pg_122.java
+++ src/main/java/chap_03/Pg_122.java
@@ -2,11 +2,11 @@ package chap_03;
 
 public class Pg_122 {
     public static void main(String[] args) {
-        char c = 'a';
+        char c = 'a';       // c 는 문자 a 이다.
         do {
-            System.out.print(c);
-            c = (char) (c+1);
+            System.out.print(c);    // c 출력
+            c = (char) (c+1);       // 아스키코드로 변환 하여 c+1 을 한 후 char 로 다시 전환 하여 초기화.
         }
-        while (c <= 'z');
+        while (c <= 'z');       //z 가 될떄 까지 반복.
     }
 }
