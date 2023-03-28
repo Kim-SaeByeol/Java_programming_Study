0e4a2b44547bfa39e81b5ec45ab49318ba236a9f
Kim-SaeByeol <toquf345@gmail.com>
2023-03-28 18:24:58 +0900

    공부용

--- src/main/java/chap_03/Pg_118.java
+++ src/main/java/chap_03/Pg_118.java
@@ -3,14 +3,14 @@ package chap_03;
 public class Pg_118 {
     public static void main(String[] args) {
         int sum = 0;
-        for(int i = 1; i<= 10; i++){
-            sum += 1;
+        for(int i = 1; i<= 10; i++){        //10번 반복
+            sum += i;       //sum 은 i의 변한 값을 모두 더함.
             System.out.print(i);
-            if(i<=9){
+            if(i<=9){       // 1 ~ 9 사이에 +를 붙입니다.
                 System.out.print("+");
             }else {
-                System.out.print("=");
-                System.out.print(sum);
+                System.out.print("=");      //10까지 오면 = 을 붙인다.
+                System.out.print(sum);      //1~10 까지 더 한 값을 출력한다.
             }
         }
     }
