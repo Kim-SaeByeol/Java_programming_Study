e2f6dda85b9d70cc5e3eed2bc5fdb028c0a50a65
Kim_SaeByeol <toquf345@gmail.com>
2023-03-30 23:59:13 +0900

    Study

--- src/main/java/chap_03/Pg_125.java
+++ src/main/java/chap_03/Pg_125.java
@@ -13,7 +13,7 @@ public class Pg_125 {
             if(n <= 0){
                 continue;       //continue는 반복문을 벗어나지 않고 다음 반복으로 넘어가게 한다. 음수, 0의 정수 이면 무시하고 다시 반복.
             }else {
-                sum += n;       //정수의 값을 모두 더하고 초기화 한다.
+                sum += n;       // 정수의 값을 모두 더하고 초기화 한다.
             }
         }
         System.out.println("양수의 합은 " + sum);     //정수의 값을 모두 더한 sum 을 출력한다.
