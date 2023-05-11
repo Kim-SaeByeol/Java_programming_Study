d9042b6b9c05dd24abbfa0a745ecece3a582879e
Kim-SaeByeol <toquf345@gmail.com>
2023-05-11 23:36:56 +0900

    23.05.11 OpenChallenge 문제 풀이 살짝

---
+++ src/main/java/Exercise/Pg_164_14.java
@@ -0,0 +1,30 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Pg_164_14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String course[] = {"Java", "C++", "HTML5", "컴퓨터구조", "안드로이드"};
+        int score[]  = {95, 88, 76, 62, 55};
+        String name="";
+
+        while(true) {
+            System.out.print("과목 이름 >> ");
+            name = sc.next();
+            if(name.equals("그만"))
+                break;
+            int i = 1;
+            for(i=0;i<course.length; i++) {
+                if(course[i].equals(name)) {
+                    System.out.println(course[i]+"의 점수는 "+score[i]);
+                    break;
+                }
+            }
+            if(i == course.length) {
+                System.out.println("없는 과목입니다.");
+            }
+        }
+        sc.close();
+    }
+}
---
+++ src/main/java/Exercise/Pg_164_15.java
@@ -0,0 +1,23 @@
+package Exercise;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Pg_164_15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (; ; ) {
+            System.out.print("곱하고자 하는 두 수 입력 >> ");
+            try {
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+                System.out.print(n + "x" + m + "=" + n * m);
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("실수는 입력하면 안됩니다.");
+                sc.nextLine();
+            }
+        }
+        sc.close();
+    }
+}
