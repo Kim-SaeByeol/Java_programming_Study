5e4e79e989346b07ae9b37a5172b99e3d69bd735
Kim-SaeByeol <toquf345@gmail.com>
2023-05-12 16:26:06 +0900

    수정

--- src/main/java/Exercise/Pg_167_11.java
+++ src/main/java/Exercise/Pg_167_11.java
@@ -3,14 +3,14 @@ package Exercise;
 public class Pg_167_11 {
     public static void main(String[] args) {
         System.out.print("명령행 인자 : ");
-        for(int i = 0; i < args.length; i++)
+        for (int i = 0; i < args.length; i++)
             System.out.print(args[i] + " ");
         System.out.println();
 
         int sum = 0;
-        for (int i = 0; i < args.length; i++){
+        for (int i = 0; i < args.length; i++) {
             sum += Integer.parseInt(args[i]);
+            System.out.println("명령행 인자의 평균 값 : " + sum / args.length);
         }
-        System.out.println("명령행 인자의 평균 값 : " + sum/ args.length);
     }
 }
