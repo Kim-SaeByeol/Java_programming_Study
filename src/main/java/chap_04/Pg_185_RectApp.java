8eb991ad047e9b76909decaf73bce354059a0515
Kim-SaeByeol <toquf345@gmail.com>
2023-05-19 23:55:52 +0900

    23.05.19 클래스와 객체

---
+++ src/main/java/chap_04/Pg_185_RectApp.java
@@ -0,0 +1,14 @@
+package chap_04;
+
+import java.util.Scanner;
+    public class Pg_185_RectApp {
+        public static void main(String[] args) {
+            Pg_185_Rectangle rect = new Pg_185_Rectangle();
+            Scanner sc = new Scanner(System.in);
+            System.out.print(">>");
+            rect.width = sc.nextInt();
+            rect.height = sc.nextInt();
+            System.out.println("사각형의 면적은 " + rect.getArea());
+            sc.close();
+        }
+    }
---
+++ src/main/java/chap_04/Pg_185_Rectangle.java
@@ -0,0 +1,10 @@
+package chap_04;
+
+public class Pg_185_Rectangle {
+    int width;
+    int height;
+
+    public int getArea() {
+        return width * height;
+    }
+}
